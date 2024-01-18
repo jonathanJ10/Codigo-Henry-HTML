691508e451f9eadc042f5a85fbc6497a58bf9ffc
Johathan <johathan.penagos@gmail.com>
2024-01-18 16:29:15 -0500

    segundo commit

---
+++ scrip.js
@@ -0,0 +1,20 @@
+const cloud = document.getElementById("cloud");
+const barralateral = document.querySelector(".barra-lateral");
+const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+ 
+palanca.addEventListener("click", ()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+});
+ 
+cloud.addEventListener("click",()=>{
+    barralateral.classList.toggle("mini-barra-lateral");
+   
+   spans.forEach((span)=>{
+        span.classList.toggle("oculto");
+});
+   
+});
